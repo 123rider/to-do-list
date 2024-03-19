9e4bb660beacd90fd08569bbb5fec7185a7a44b9
root <duybvtnm65@outlook.com.vn>
2024-03-19 16:03:16 +0700

    add login page

--- to-do-list-frontend/src/Components/login.tsx
+++ to-do-list-frontend/src/Components/login.tsx
@@ -1,29 +1,40 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import axios from "axios"
 
 interface login_data {
-    email : string|undefined;
-    password : string|undefined;
+    Email : string|undefined;
+    Password : string|undefined;
 }
 
 const Login: React.FC = ()=> {
     document.title="Login"
     const email =useRef<HTMLInputElement>(null)
     const password = useRef<HTMLInputElement>(null)
-
+    const [response,set_response]=useState(null)
     
-    const login= async (data:login_data) =>{
-        
+    const login = async (data:login_data) =>{
+        try{
+            const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}auth/login/`,data)
+            await set_response(response.data)
+         
+        }catch(e:any){
+            if (e.response?.status === 400){
+                console.log(e.response.data)
+            }
+        }
     }
-
     return <div className="container" >
         <form onSubmit={(e: React.FormEvent) => {
             e.preventDefault()
             const user_data: login_data = {
-                email: email.current?.value,
-                password: password.current?.value,
+                Email: email.current?.value,
+                Password: password.current?.value,
             }
-
-
+            if(user_data.Email && user_data.Password) {
+                login(user_data)
+                console.log(response)
+            }
+            
         }
 }>
             <div className="mb-3">
